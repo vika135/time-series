9e4d4e799f6b238041d043deee55b452475765db
vika135 <ptizza.music@yandex.ru>
2023-03-03 16:13:36 +0600

    feature: added toggle for log/linear scale

--- src/app/components/chart/Chart.tsx
+++ src/app/components/chart/Chart.tsx
@@ -8,6 +8,7 @@ export default function Chart() {
     const {values} = useContext(ValuesContext)
     const svgRef = useRef();
     const [dimensions, setDimensions] = useState({width: 0, height: 0});
+    const [useSymlogScale, setUseSymlogScale] = useState(true);
 
     useEffect(() => {
         const svg = d3.select(svgRef.current);
@@ -18,9 +19,11 @@ export default function Chart() {
             .range([margin.left, dimensions.width - margin.right])
             .domain(d3.extent(values, d => d.time));
 
-        const y = d3.scaleLinear()
-            .range([dimensions.height - margin.bottom, margin.top])
-            .domain(d3.extent(values, d => d.value))
+        const y = useSymlogScale ?
+            d3.scaleSymlog().range([dimensions.height - margin.bottom, margin.top])
+                .domain(d3.extent(values, d => d.value))
+            : d3.scaleLinear().range([dimensions.height - margin.bottom, margin.top])
+                .domain(d3.extent(values, d => d.value))
 
         const xAxis = g => g
             .attr('transform', `translate(0, ${dimensions.height - margin.bottom})`)
@@ -28,7 +31,10 @@ export default function Chart() {
 
         const yAxis = g => g
             .attr('transform', `translate(${margin.left}, 0)`)
-            .call(d3.axisLeft(y).ticks(5))
+            .call(useSymlogScale ?
+                d3.axisLeft(y).ticks(5, ".1s")
+                : d3.axisLeft(y)
+            );
 
         const line = d3.line()
             .defined(d => !isNaN(d.value))
@@ -43,7 +49,7 @@ export default function Chart() {
 
         svg.select('.y-axis')
             .call(yAxis);
-    }, [values, dimensions]);
+    }, [values, dimensions, useSymlogScale]);
 
     useEffect(() => {
         function handleResize() {
@@ -59,6 +65,10 @@ export default function Chart() {
         return () => window.removeEventListener("resize", throttledResize);
     }, []);
 
+    const handleToggleScale = () => {
+        setUseSymlogScale(!useSymlogScale);
+    }
+
     return (
         <div className='chart' ref={svgRef}>
             <svg className='chart__svg' width={dimensions.width} height={dimensions.height}>
@@ -68,6 +78,11 @@ export default function Chart() {
                 <g className='x-axis' />
                 <g className='y-axis' />
             </svg>
+            <div>
+                <button onClick={handleToggleScale}>
+                    {useSymlogScale ? 'Switch to Linear Scale' : 'Switch to Log Scale'}
+                </button>
+            </div>
         </div>
     )
 }
